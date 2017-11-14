73ac7537c172416c50439aed645696fa4422758a
sbaxter95 <sbaxter95@outlook.com>
2017-11-14 09:12:17 +0000

    added xml file

--- api.rb
+++ api.rb
@@ -1,6 +1,5 @@
 require_relative 'spec/spec_helper'
 require 'sinatra/base'
-
 require 'nokogiri'
 
 class APIApp < Sinatra::Base
@@ -12,12 +11,12 @@ class APIApp < Sinatra::Base
 
   #Display all devices
   get '/devices' do
-
+    content_type 'text/xml'
   end
 
   #Display notes for single device
   get '/devices/:device' do
-
+    content_type 'text/xml'
   end
 
 end
