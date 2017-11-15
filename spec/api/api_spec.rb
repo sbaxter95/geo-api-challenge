a331996a19242295dcce1b387adb31862bb9f6b9
sbaxter95 <sbaxter95@outlook.com>
2017-11-15 14:38:43 +0000

    modified and organised test file

--- spec/api/api_spec.rb
+++ spec/api/api_spec.rb
@@ -5,27 +5,79 @@ require_relative '../../homepage.rb'
 
 describe Routes do
 
-  it 'should be able to show the homepage' do
-    get '/'
-      expect(last_response).to be_ok
+  before(:all) do
+    @home = Homepage.new
+		@devices = AllDevices.new('mini-schema.xml')
+    @find = FindDevice.new('mini-schema.xml')
+	end
+
+  context 'homepage' do
+
+    it 'should be able to show the homepage' do
+      get '/'
+        expect(last_response).to be_ok
+    end
+
+    it 'should show the correct homepage content' do
+      get '/'
+        expect(@home.show_homepage_content).to eq('Welcome to the Devices API')
+    end
+
+
   end
 
-  it 'should show the correct homepage content' do
-    get '/'
-      @home = Homepage.new
-      expect(@home.show_homepage_content).to eq('Welcome to the Devices API')
-  end
+  context 'all devices page' do
 
-  it 'should show all of the devices at the correct url' do
-    get '/devices'
-      @devices = AllDevices.new('mini-schema.xml')
-      expect(@devices.all_devices).not_to be_nil
-  end
+    it 'should show all of the devices at the correct url' do
+      get '/devices'
+        expect(@devices.all_devices).not_to be_nil
+    end
 
-  it 'should show all of the devices at the correct url' do
-    get '/devices'
-      @devices = AllDevices.new('mini-schema.xml')
-      expect(@devices.all_devices.class).to eq(String)
+    it 'should show all of the devices at the correct url' do
+      get '/devices'
+        expect(@devices.all_devices.class).to eq(String)
+    end
+
+    it 'should include the word name' do
+      get '/devices'
+        expect(@devices.all_devices).to include('name')
+    end
+
+    it 'should include the word value' do
+      get '/devices'
+        expect(@devices.all_devices).to include('value')
+    end
+
+    it 'should include the word notes' do
+      get '/devices'
+        expect(@devices.all_devices).to include('notes')
+    end
+
+    it 'should have a first character of [' do
+      get '/devices'
+        expect(@devices.all_devices[0]).to eq('[')
+    end
+
+    it 'should have a last character of ]' do
+      get '/devices'
+        expect(@devices.all_devices[-1]).to eq(']')
+    end
+
+    it 'should have a second character of {' do
+      get '/devices'
+        expect(@devices.all_devices[1]).to eq('{')
+    end
+
+    it 'should have a first character of }' do
+      get '/devices'
+        expect(@devices.all_devices[-2]).to eq('}')
+    end
+
+    it 'should be able to show the devices page' do
+      get '/devices'
+        expect(last_response).to be_ok
+    end
+
   end
 
 end
