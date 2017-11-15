d017d525a1dbc8e7c034e272a1d079b7496eba98
sbaxter95 <sbaxter95@outlook.com>
2017-11-15 10:02:45 +0000

    converting to json first"

--- api.rb
+++ api.rb
@@ -4,6 +4,13 @@ require 'nokogiri'
 
 class APIApp < Sinatra::Base
 
+  def initialize(file)
+		@doc = Nokogiri::XML(File.open(file))
+		parsed = @doc.to_s
+		json = Hash.from_xml(parsed)
+		@name_array = json['document']['devices']['device']
+	end
+
   #Display homepage to the api
   get '/' do
     'Welcome to the Devices API'
@@ -11,18 +18,20 @@ class APIApp < Sinatra::Base
 
   #Display all devices
   get '/devices' do
-    content_type 'text/xml'
+    content_type 'application/json'
     doc = File.read("mini-schema.xml")
     return doc
   end
 
   #Display notes for single device
   get '/devices/:device' do
-    content_type 'text/xml'
+    content_type 'application/json'
     name = params['device']
     doc = Nokogiri::XML(File.read("mini-schema.xml"))
-    doc.at("name:contains('#{name}')").parent.to_xml
-    doc.at("notes")
+    parsed = doc.at("name:contains('#{name}')").parent.to_xml
+    # doc.xpath('//notes')
+    # string = doc.to_s
+    # puts string.split
   end
 
 end
