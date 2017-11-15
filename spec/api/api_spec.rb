27a65590b6ef86a727e1d611ea5821049350c9f3
sbaxter95 <sbaxter95@outlook.com>
2017-11-15 14:29:17 +0000

    testing homepage and all devices

--- spec/api/api_spec.rb
+++ spec/api/api_spec.rb
@@ -5,10 +5,6 @@ require_relative '../../homepage.rb'
 
 describe Routes do
 
-  before(:all) do
-    @api = Routes.new
-  end
-
   it 'should be able to show the homepage' do
     get '/'
       expect(last_response).to be_ok
@@ -17,7 +13,19 @@ describe Routes do
   it 'should show the correct homepage content' do
     get '/'
       @home = Homepage.new
-      puts @home.show_homepage_content
+      expect(@home.show_homepage_content).to eq('Welcome to the Devices API')
+  end
+
+  it 'should show all of the devices at the correct url' do
+    get '/devices'
+      @devices = AllDevices.new('mini-schema.xml')
+      expect(@devices.all_devices).not_to be_nil
+  end
+
+  it 'should show all of the devices at the correct url' do
+    get '/devices'
+      @devices = AllDevices.new('mini-schema.xml')
+      expect(@devices.all_devices.class).to eq(String)
   end
 
 end
